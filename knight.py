78c2a84c0ef68ad2d59bbd0d96bcd3807ad1b52b
Adrian Otgonzaya <adrianotgonzaya07@gmail.com>
2021-06-21 17:33:16 +0200

    knight update

--- knight.py
+++ knight.py
@@ -8,14 +8,23 @@ class Knight(Piece):
     def validMoves(self, location, board):
         validMoves = []
        
-        if location[0]+2 <= 7 location[1]+2 <= 7:
-            if board[location[0]][location[1]].islower(): # black
-                if board[location[0]+2][location[1]+1].isalpha() and board[location[0]+2][location[1]+1].islower() != board[location[0]][location[1]].islower():
-                    validMoves.append(board[location[0]+2][location[1]+1])
-                elif board[location[0]+2][location[1]+1]
-                    pass
-            else: # white
-                pass
+        if location[0]+2 <= 7 and location[1]+1 <= 7:
+            if board[location[0]+2][location[1]+1].isalpha() and board[location[0]+2][location[1]+1].islower() != board[location[0]][location[1]].islower():
+                validMoves.append((location[0]+2,location[1]+1))
+            elif not board[location[0]+2][location[1]+1].isalpha():
+                validMoves.append((location[0]+2, location[1]+1))
+
+        if location[0]+2 <= 7 and location[1]-1 <= 7:
+            if board[location[0]+2][location[1]-1].isalpha() and board[location[0]+2][location[1]-1].islower() != board[location[0]][location[1]].islower():
+                validMoves.append((location[0]+2,location[1]-1))
+            elif not board[location[0]+2][location[1]-1].isalpha():
+                validMoves.append((location[0]+2, location[1]-1))
+
+        if location[0]+2 <= 7 and location[1]-1 <= 7:
+            if board[location[0]+2][location[1]-1].isalpha() and board[location[0]+2][location[1]-1].islower() != board[location[0]][location[1]].islower():
+                validMoves.append((location[0]+2,location[1]-1))
+            elif not board[location[0]+2][location[1]-1].isalpha():
+                validMoves.append((location[0]+2, location[1]-1))
 
         return validMoves
 
