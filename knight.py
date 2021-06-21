48001ead999dca9f7faf094c18f4380aab639887
Adrian Otgonzaya <adrianotgonzaya07@gmail.com>
2021-06-21 18:37:44 +0200

    knight movement

--- knight.py
+++ knight.py
@@ -9,23 +9,56 @@ class Knight(Piece):
         validMoves = []
        
         if location[0]+2 <= 7 and location[1]+1 <= 7:
-            if board[location[0]+2][location[1]+1].isalpha() and board[location[0]+2][location[1]+1].islower() != board[location[0]][location[1]].islower():
+            if board[location[0]+2][location[1]+1].isalpha() and board[location[0]+2][location[1]+1].islower() != board[location[0]][location[1]].islower(): # capture
                 validMoves.append((location[0]+2,location[1]+1))
             elif not board[location[0]+2][location[1]+1].isalpha():
                 validMoves.append((location[0]+2, location[1]+1))
 
         if location[0]+2 <= 7 and location[1]-1 <= 7:
-            if board[location[0]+2][location[1]-1].isalpha() and board[location[0]+2][location[1]-1].islower() != board[location[0]][location[1]].islower():
+            if board[location[0]+2][location[1]-1].isalpha() and board[location[0]+2][location[1]-1].islower() != board[location[0]][location[1]].islower(): # capture
                 validMoves.append((location[0]+2,location[1]-1))
             elif not board[location[0]+2][location[1]-1].isalpha():
                 validMoves.append((location[0]+2, location[1]-1))
 
-        if location[0]+2 <= 7 and location[1]-1 <= 7:
-            if board[location[0]+2][location[1]-1].isalpha() and board[location[0]+2][location[1]-1].islower() != board[location[0]][location[1]].islower():
-                validMoves.append((location[0]+2,location[1]-1))
-            elif not board[location[0]+2][location[1]-1].isalpha():
-                validMoves.append((location[0]+2, location[1]-1))
+        if location[0]-2 <= 7 and location[1]-1 <= 7:
+            if board[location[0]-2][location[1]-1].isalpha() and board[location[0]-2][location[1]-1].islower() != board[location[0]][location[1]].islower(): # capture
+                validMoves.append((location[0]-2,location[1]-1))
+            elif not board[location[0]-2][location[1]-1].isalpha():
+                validMoves.append((location[0]-2, location[1]-1))
 
+        if location[0]-2 <= 7 and location[1]+1 <= 7:
+            if board[location[0]-2][location[1]+1].isalpha() and board[location[0]-2][location[1]+1].islower() != board[location[0]][location[1]].islower(): # capture
+                validMoves.append((location[0]-2,location[1]+1))
+            elif not board[location[0]-2][location[1]+1].isalpha():
+                validMoves.append((location[0]-2, location[1]+1))
+        
+        if location[0]-1 <= 7 and location[1]-2 <= 7:
+            if board[location[0]-1][location[1]-2].isalpha() and board[location[0]-1][location[1]-2].islower() != board[location[0]][location[1]].islower(): # capture
+                validMoves.append((location[0]-1, location[1]-2))
+            elif not board[location[0]-1][location[1]].isalpha():
+                validMoves.append((location[0]-1, location[1]-2))
+
+        if location[0]+1 <= 7 and location[1]-2 <= 7:
+            if board[location[0]+1][location[1]-2].isalpha() and board[location[0]+1][location[1]-2].islower() != board[location[0]][location[1]].islower(): # capture
+                validMoves.append((location[0]+1, location[1]-2))
+            elif not board[location[0]+1][location[1]].isalpha():
+                validMoves.append((location[0]+1, location[1]-2))
+
+        if location[0]-1 <= 7 and location[1]+2 <= 7:
+            if board[location[0]-1][location[1]+2].isalpha() and board[location[0]-1][location[1]+2].islower() != board[location[0]][location[1]].islower(): # capture
+                validMoves.append((location[0]-1, location[1]+2))
+            elif not board[location[0]-1][location[1]].isalpha():
+                validMoves.append((location[0]-1, location[1]+2))
+
+        if location[0]+1 <= 7 and location[1]+2 <= 7:
+            if board[location[0]+1][location[1]+2].isalpha() and board[location[0]+1][location[1]+2].islower() != board[location[0]][location[1]].islower(): # capture
+                validMoves.append((location[0]+1, location[1]+2))
+            elif not board[location[0]+1][location[1]].isalpha():
+                validMoves.append((location[0]+1, location[1]+2))
+
+
+
+        print(len(validMoves))
         return validMoves
 
 loc = [3,3]
